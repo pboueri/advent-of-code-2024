4e29e3d8d1f449102806dcc228fccf23680bdc08
Patrick Boueri <patrick.boueri@gmail.com>
2024-12-14 13:14:22 -0600

    solved day 9

--- adventurust/src/bin/december_9.rs
+++ adventurust/src/bin/december_9.rs
@@ -17,6 +17,8 @@ Approach:
 */
 
 
+const MAX_SIZE: i32 = 9;
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
@@ -81,7 +83,7 @@ fn reallocate_files(file_layout: &mut Vec<Option<u32>>) {
 
 fn build_span_set (file_layout: &Vec<Option<u32>>) -> HashMap<i32, Vec<usize>>{
     let mut empty_spots = HashMap::new();
-    for span_size in 0..9 {
+    for span_size in 0..10 {
         empty_spots.insert(span_size as i32, Vec::<usize>::new());
     }
     let mut offset = 0;
@@ -109,34 +111,62 @@ fn build_span_set (file_layout: &Vec<Option<u32>>) -> HashMap<i32, Vec<usize>>{
 
 fn get_file_spans(file_layout: &Vec<Option<u32>>) -> Vec<(usize, usize)>{
     let mut file_sizes: Vec<(usize, usize)> = Vec::new();
-    let mut span_size = 0;
-    let mut offset = 0;
-    let mut new_span = true;
-    let mut new_file_id = 0;
-    let mut old_file_id = 0;
+    let mut span_size = 1;
+    let mut offset = 1;
+    let mut new_file_id = file_layout[0];
+    let mut old_file_id = file_layout[0];
     while offset < file_layout.len() {
-        if file_layout[offset].is_some(){
-            new_file_id = file_layout[offset].unwrap();
-        }
-        if new_file_id == old_file_id && file_layout[offset].is_some() {
-            new_span = false;
-            span_size += 1;
-        } else {
-            if !new_span {
+        new_file_id = file_layout[offset];
+
+        match (old_file_id, new_file_id) {
+            (Some(old_file), Some(new_file)) if old_file == new_file => {
+                span_size += 1;
+            },
+            (Some(old_file), Some(new_file)) if old_file != new_file => {
                 file_sizes.push((offset - span_size, span_size));
+                span_size = 1;
+            },
+            (None, Some(new_file)) => {
+                span_size = 1;
+            },
+            (Some(_), None) => {
+                file_sizes.push((offset - span_size, span_size));
+                span_size = 1;
+            },
+            (None, None) => {
+            },
+            _ => {
+                panic!("Unexpected file state")
             }
-            new_span = true;
-            span_size = 1;
         }
+        
         offset += 1;
         if offset == file_layout.len() {
-            file_sizes.push((offset - span_size, span_size));
+            if old_file_id.is_some() {
+                file_sizes.push((offset - span_size, span_size));
+            }
+            break;
         }
         old_file_id = new_file_id;
     }
     file_sizes
 }
 
+
+fn get_earliest_slot_big_enough(file_start: usize, file_size: usize, empty_spots: &HashMap<i32, Vec<usize>>) -> (usize, usize) {
+    let mut earliest_slot = (file_start, file_size);
+    for slot_size in (file_size as i32)..(MAX_SIZE + 1) {
+        let available_slots = empty_spots.get(&slot_size);
+        if available_slots.unwrap().len() > 0{
+            // if is earlier
+            let empty_spot = available_slots.unwrap().last().unwrap();
+            if empty_spot <= &earliest_slot.0 {
+                earliest_slot = (*empty_spot, (slot_size as usize));
+            }
+        }
+    }
+    earliest_slot
+}
 
 fn defrag_files(file_layout: &mut Vec<Option<u32>>) {
     let mut empty_spots = build_span_set(file_layout);
@@ -146,29 +176,19 @@ fn defrag_files(file_layout: &mut Vec<Option<u32>>) {
     for file_size in file_sizes.into_iter().rev() {
         let mut start = file_size.0;
         let size = file_size.1;
+        debug!("Avalilable spots: {:?}", empty_spots);
         debug!("Start: {start}, Size: {size}, file_id {:?}", file_layout[start]);
-        //TODO: Move to spots big enough (same or BIGGER)
-        //TODO: Update empty spots to be smaller than they used to be (but still present)
-        let mut earliest_slot = (file_layout.len() as usize, size);
-        for i in size..9 {
-            if empty_spots.get(&(i as i32)).unwrap().len() > 0 {
-                if empty_spots.get(&(i as i32)).unwrap().last().unwrap() < &earliest_slot.0 {
-                    earliest_slot = (i, *empty_spots.get(&(i as i32)).unwrap().last().unwrap());
-                }
-            }
-        }
-
-        if earliest_slot.0 == 0 || earliest_slot.0 > start {
+        let slot_to_insert = get_earliest_slot_big_enough(start, size, &empty_spots);
+        if slot_to_insert.0 == start {
             debug!("No empty spots big enough");
             continue;
         }
 
-        debug!("Earliest slot: {:?}", earliest_slot);
-        
-        let mut offset: usize = earliest_slot.1;
+        // Move the file
+        debug!("Slot to insert: {:?}", slot_to_insert);
+        let mut offset: usize = slot_to_insert.0;
         let mut file_number = file_layout[start].unwrap();
         
-        empty_spots.get_mut(&(size as i32)).unwrap().pop();
         for _ in 0..size {
             file_layout[offset] = Some(file_number);
             offset += 1;
@@ -176,6 +196,14 @@ fn defrag_files(file_layout: &mut Vec<Option<u32>>) {
         for _ in 0..size {
             file_layout[start] = None;
             start += 1;
+        }
+
+        // Change slot size
+        empty_spots.get_mut(&(slot_to_insert.1 as i32)).unwrap().pop();
+        // If there are remaining empty spaces add them to the empty spots
+        if slot_to_insert.1 - size >  0 {
+            empty_spots.get_mut(&(slot_to_insert.1 as i32 - size as i32)).unwrap().push(offset);
+            empty_spots.get_mut(&(slot_to_insert.1 as i32 - size as i32)).unwrap().sort_by(|a, b| b.cmp(a));
         }
         
     }
@@ -217,9 +245,7 @@ fn main(){
     info!("Answer 1: {}", checksum(&naive_reallocate));
 
     let mut defrag_reallocate = vector.clone();
-    print_file_layout(&defrag_reallocate);
     defrag_files(&mut defrag_reallocate);
-    print_file_layout(&defrag_reallocate);
     info!("Answer 2: {}", checksum(&defrag_reallocate));
     
 }

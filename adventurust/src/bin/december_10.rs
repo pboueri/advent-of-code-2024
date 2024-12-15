5c5e44062688adfe858e779fe3d4564fdf1dac79
Patrick Boueri <patrick.boueri@gmail.com>
2024-12-15 11:32:58 -0600

    solved day 10

--- adventurust/src/bin/december_10.rs
+++ adventurust/src/bin/december_10.rs
@@ -42,7 +42,8 @@ impl Point {
 type TrailMapLinks = HashMap<Point, HashSet<Point>>;
 
 struct TrailMap {
-    links: TrailMapLinks,
+    ascents: TrailMapLinks,
+    descents: TrailMapLinks,
     raw_map: Vec<Vec<i32>>,
     summits: HashSet<Point>,
     trailheads: HashSet<Point>,
@@ -58,6 +59,10 @@ impl TrailMap {
             println!();
         }
     }
+    
+    fn get_height(&self, point: &Point) -> i32 {
+        self.raw_map[point.y as usize][point.x as usize]
+    }
 }
 
 
@@ -69,14 +74,16 @@ struct Cli {
 }
 
 fn build_map(file_map: &Vec<Vec<i32>>) -> TrailMap {
-    let mut links = HashMap::new();
+    let mut ascents = HashMap::new();
+    let mut descents = HashMap::new();
     let mut summits = HashSet::new();
     let mut trailheads = HashSet::new();
 
     for (y, line) in file_map.iter().enumerate() {
         for (x, current_height) in line.iter().enumerate() {
             let point = Point { x: x as i32, y: y as i32 };
-            let mut links_for_point = HashSet::new();
+            let mut ascents_for_point = HashSet::new();
+            let mut descents_for_point = HashSet::new();
             if current_height == &TRAILHEAD {
                 trailheads.insert(point.clone());
             }
@@ -97,16 +104,25 @@ fn build_map(file_map: &Vec<Vec<i32>>) -> TrailMap {
                 if let Some(line) = file_map.get(new_point.y as usize) {
                     if let Some(new_height) = line.get(new_point.x as usize) {
                         if *new_height==current_height+1 {
-                            links_for_point.insert(new_point);
+                            ascents_for_point.insert(new_point);
+                        }
+                    }
+                }
+                if let Some(line) = file_map.get(new_point.y as usize) {
+                    if let Some(new_height) = line.get(new_point.x as usize) {
+                        if *new_height==current_height-1 {
+                            descents_for_point.insert(new_point);
                         }
                     }
                 }
             }
-            links.insert(point, links_for_point);
+            ascents.insert(point, ascents_for_point);
+            descents.insert(point, descents_for_point);
         }
     }
     TrailMap {
-        links,
+        ascents: ascents,
+        descents: descents,
         raw_map: file_map.clone(),
         summits,
         trailheads,
@@ -130,17 +146,21 @@ fn get_file_map(file_path: String) -> TrailMap {
 
 fn paths_to_summit(current_point: &Point, visited_points: &mut HashSet<Point>, trail_map: &TrailMap) -> i32 {
     visited_points.insert(current_point.clone());
+    debug!("Visited Points: {:?}: {:?}", visited_points.len(), current_point);
     if trail_map.summits.contains(&current_point) {
+        debug!("Found summit: {:?}", current_point);
         return 1;
     }
-    let next_points = trail_map.links.get(&current_point);
+    let next_points = trail_map.ascents.get(&current_point);
     if next_points.is_none() {
         return 0;
     }
     let mut sum = 0;
+    debug!("\t Next points: {:?}", next_points);
     for possible_next_point in next_points.unwrap() {
         if visited_points.contains(possible_next_point) {
-            return 0
+            debug!("Already visited point: {:?}", possible_next_point);
+            continue;
         }
         sum += paths_to_summit(possible_next_point, visited_points, trail_map);
     }
@@ -149,6 +169,23 @@ fn paths_to_summit(current_point: &Point, visited_points: &mut HashSet<Point>, t
 
 
 
+fn trail_rating(current_point: &Point, trailhead_ratings: &mut HashMap<Point, i32>, trail_map: &TrailMap) {
+    if trail_map.trailheads.contains(&current_point) {
+        debug!("Found trailhead: {:?}", current_point);
+        *trailhead_ratings.get_mut(current_point).unwrap()+=1;
+    }
+    let next_points = trail_map.descents.get(&current_point);
+    if next_points.is_none() {
+        return;
+    }
+    
+    debug!("\t Next points: {:?}", next_points);
+    for possible_next_point in next_points.unwrap() {
+        trail_rating(possible_next_point, trailhead_ratings, trail_map);
+    }
+    
+}
+
 fn main(){
     env_logger::init();
 
@@ -156,9 +193,10 @@ fn main(){
     let file_path = args.file_path;
     let file_map = get_file_map(file_path);
     
-    file_map.print();
-    debug!("Trailheads: {:?}", file_map.trailheads.len());
-    debug!("Summits: {:?}", file_map.summits.len());
+    //file_map.print();
+    debug!("Trailheads: {:?}", file_map.trailheads);
+    debug!("Summits: {:?}", file_map.summits);
+    debug!("Ascents: {:?}", file_map.ascents.iter().map(|x| x.1.len()).sum::<usize>());
     let mut total_trail_score = 0;
     for trailhead in file_map.trailheads.iter() {
         let mut visited_points: HashSet<Point> = HashSet::new();
@@ -167,5 +205,14 @@ fn main(){
         debug!("Trailhead: {:?} has {} paths to summit", trailhead, trail_score);
     }
     info!("Total trail score: {}", total_trail_score);
+
+    let mut trailhead_ratings: HashMap<Point,i32> = HashMap::new();
+    for trailhead in file_map.trailheads.iter() {
+       trailhead_ratings.insert(trailhead.clone(),0 as i32);
+    }
+    for summit in file_map.summits.iter() {
+        trail_rating(summit, &mut trailhead_ratings, &file_map);
+    }
+    info!("Trailhead Ratings: {:?}", trailhead_ratings.iter().map(|x| x.1).sum::<i32>());
 
 }
